0398fcd17f631eb8985de46646c85be6084ddca1
nem0nke <154655563+nem0nke@users.noreply.github.com>
2025-08-26 22:42:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+from pprint import pprint
+
 names = open("names.txt", "r").read().split()
 
 #<S> == start of name; <E> == end of name
-names = [['<S>'] + list(i) + ['<E>'] for i in names]
+#names = [['<S>'] + list(i) + ['<E>'] for i in names]
 
-print(names[:10])
+listvalue = {}
 
+for i in names:
+    names = ['<S>'] + list(i) + ['<E>']
+    for char1, char2 in zip(names, names[1:]):
+        bigram = (char1, char2)
+        listvalue[bigram] = listvalue.get(bigram, 0) + 1
 
+sorted_items = sorted(listvalue.items(), key=lambda x: x[1], reverse=True)
 
-#function bigrams()
+pprint(sorted_items)
